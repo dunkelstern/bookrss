0d86fcfd98800048fdf00aff5d6b20052556be5e
Johannes Schriewer <hallo@dunkelstern.de>
2018-03-01 02:50:52 +0100

    WIP: clap command line parser for cli tool

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -15,10 +15,33 @@ extern crate r2d2;
 // Chrono
 extern crate chrono;
 
+// cmdline parser
+#[macro_use]
+extern crate clap;
+
 // app
 extern crate lib;
-//pub mod models;
-//pub mod views;
+use lib::settings::Settings;
+
 
 fn main() {
+    let matches = clap_app!(bookrss =>
+        (version: "1.0")
+        (author: "Johannes Schriewer <hallo@dunkelstern.de>")
+        (about: "Audiobook RSS builder cli tool")
+        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
+        (@subcommand import =>
+            (about: "import audio file and read it's metadata into the DB")
+            (@arg INPUT: +required "Sets the input file to use")
+        )
+    ).get_matches();
+
+    let config_file = matches.value_of("CONFIG").unwrap_or("bookrss");
+
+    match Settings::new(config_file) {
+        Ok(settings) => { 
+            println!("run forest run: {:?}", config_file);
+        },
+        Err(error) => { println!("Config error: {:?}", error); }
+    };
 }
